99f9d61a7f39636e4a5574153952d8a3330c4a6f
andyferraro <andrea.ferraro@cnaf.infn.it>
2013-11-19 12:10:24 +0100

    test

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
