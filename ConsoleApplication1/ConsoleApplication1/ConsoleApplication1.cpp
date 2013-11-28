7a44a45d0a52badba3e60e273e6ae0d1dca3389a
andyferraro <andrea.ferraro@cnaf.infn.it>
2013-11-28 16:08:52 +0100

    Reordened code

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,55 +16,41 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	// Test rand()
-	srand(time(0));
+	/*srand(time(0));
 	cout << "inizio" << endl;
 	cout << "RAND_MAX: " << RAND_MAX << endl;
 	for (int i = 0; i<MAXLOOP; i++) {
 		int k = rand();
 		cout << i << ": " << k << endl;
 	}
-	cout << "fine" << endl;
+	cout << "fine" << endl;*/
 
-	// CShape, CTriangle, CRectangle test
-	CShape cshape;
-	cout << cshape.getArea() << endl;
+	// CShape, CTriangle, CRectangle test	
 	CTriangle t(1, 4);
 	CRectangle r(2, 3);
-		
-	cout << "t.getVName(): " << t.getVName() << ", ";
-	cout << "r.getVName(): " << r.getVName() << ", " << endl;
- 
-	cout << "t.getName(): " << t.getName() << ", ";
-	cout << "r.getName(): " << r.getName() << ", " << endl;
-
 	CShape *tp = &t, *rp = &r;
-	
-	cout << "tp.getVName(): " << tp->getVName() << ", ";
-	cout << "rp.getVName(): " << rp->getVName() << ", "<< endl; 
-	cout << "tp.getName(): " << tp->getName() << ", ";
-	cout << "rp.getName(): " << rp->getName() << ", " << endl;
-
-
 	vector<CShape> s_vector;
-	s_vector.push_back(t);
-	s_vector.push_back(r);
+	vector<CShape*> sp_vector;
+	CShape *s_array[2];
+
+	s_vector.push_back(t); s_vector.push_back(r);
+	sp_vector.push_back(&t); sp_vector.push_back(&r);
+	s_array[0] = &t; s_array[1] = &r;
+
+	cout << "t.getVName(): " << t.getVName() << "r.getVName(): " << r.getVName() << ", " << endl;
+	cout << "t.getName(): " << t.getName() << "r.getName(): " << r.getName() << ", " << endl;
+	cout << "tp.getVName(): " << tp->getVName() << "rp.getVName(): " << rp->getVName() << ", "<< endl; 
+	cout << "tp.getName(): " << tp->getName() << "rp.getName(): " << rp->getName() << ", " << endl;
+
 	for (int i = 0; i < s_vector.size(); i++)
 		cout << "s_vector: " << s_vector[i].getVName() << endl;
-	vector<CShape*> sp_vector;
-	/*CShape *ps1 = &t;
-	CShape *ps2 = &r;*/
-	sp_vector.push_back(&t);
-	sp_vector.push_back(&r);
+	
 	for (CShape* p : sp_vector)
 		cout << "sp_vector: " << p->getVName() << endl;
-	CShape *s_array[2];
-	s_array[0] = &t;
-	s_array[1] = &r;
-
+	
 	for (int i = 0; i < 2; i++)
 		cout << "s_array: " << s_array[i]->getVName() << endl;
 
-	cout << "Helloworld!" << endl;
 	return 0;
 }
 
