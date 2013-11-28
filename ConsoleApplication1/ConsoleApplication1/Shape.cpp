12d205fa5c17ee0634bb3a977ac62aaac56ca757
andyferraro <andrea.ferraro@cnaf.infn.it>
2013-11-28 14:12:29 +0100

    test

--- ConsoleApplication1/ConsoleApplication1/Shape.cpp
+++ ConsoleApplication1/ConsoleApplication1/Shape.cpp
@@ -2,11 +2,34 @@
 #include "Shape.h"
 
 
+CShape::CShape(int x, int y) : width(x), height(y)
+{
+}
+
 CShape::CShape()
 {
 }
 
-
 CShape::~CShape()
 {
+}
+
+std::string CShape::getVName()
+{
+	return "VCShape";
+}
+
+std::string CShape::getName()
+{
+	return "CShape";
+}
+
+int CShape::getArea()
+{
+	return -1;
+}
+
+int CShape::getPerimeter()
+{
+	return -1;
 }
--- ConsoleApplication1/ConsoleApplication1/Shape.h
+++ ConsoleApplication1/ConsoleApplication1/Shape.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <string>
+
 class CShape
 {
+	int width, height;
+	int area;
+	int perimeter;
 public:
 	CShape();
+	CShape(int, int);
 	~CShape();
+	virtual std::string getVName();
+	std::string getName();
+	virtual int getArea();
+	virtual int getPerimeter();
 };
 
